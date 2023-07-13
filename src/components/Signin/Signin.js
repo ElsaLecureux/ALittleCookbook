82e693c7b9a578fcc15facb15ed1bde89827b9c9
ElsaLecureux <elsa.lecureux@gmail.com>
2023-07-13 16:23:34 +0200

    pair programming starting adding sign in

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,20 +1,45 @@
 import './Signin.scss';
+import axios from 'axios';
+/* const getInfosAPI = async () => {
+  try {
+      const response = await axios.get("http://localhost:5000/recipes")
+     // console.log('response:',response);
+      const data = response.data;
+      setRecipes(data);
+  } catch (error) {
+      console.log('error :', error);
+  }
+  
+}
+useEffect(() => {
+  getInfosAPI();
+}, []);  */
 
 function Signin() {
   const signInUser =  async (e) => {
     e.preventDefault();
-    console.log('inside signInUser');
+    const email = e.target[0].value; 
+    const password =  e.target[1].value;
+    try {
+      const response = await axios.post("http://localhost:5000/user", {email, password})
+     // console.log('response:',response);
+      const data = response.data;
+      
+  } catch (error) {
+      console.log('error :', error);
+  }
+    
   }
   return (
     <div className="signin_container content">
        <form action="" method="post" onSubmit={signInUser}>
         <div>
             <label htmlFor="email">Email</label>
-            <input type="text" id='email' name='email'/>
+            <input type="email" id='email' name='email'/>
         </div>
         <div>
             <label htmlFor="password">Password</label>
-            <input type="text" id='password' name='password'/>
+            <input type="password" id='password' name='password'/>
         </div>      
         <input type="submit" value="Sign In"/>
        </form>
